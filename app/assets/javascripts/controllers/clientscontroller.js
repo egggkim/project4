375b09ec750c33ea0596f37833058d397d9e63ae
Karen S Kim <karenskim19@gmail.com>
2015-02-17 16:31:20 -0800

    add auto reload on new client submit

--- app/assets/javascripts/controllers/clientscontroller.js
+++ app/assets/javascripts/controllers/clientscontroller.js
@@ -9,6 +9,7 @@ angular
 
     self.clientList   = [];  
     self.addClient    = addClient;
+    // self.updateClient = updateClient;
 
     $http.get("/api/clients").
       success(function(data, status, headers, config) {
@@ -25,8 +26,34 @@ angular
           'email': self.email,
           'phone': self.phone,
         }
+      })
+      .success(function(data) {
+        self.clientList;
+        window.location.reload();
+      })
+      .error(function(data, status) {
+        console.log(status)
+        $('#client-error').text("There was an issue adding this client. Please make sure all fields have been input correctly and try again.")
       });
     }
 
+    // function updateClient() {
+    //   $http({
+    //     url: "/api/clients",
+    //     method: "PUT",
+    //     data: {
+    //       'first_name': self.first_name,
+    //       'last_name': self.last_name,
+    //       'email': self.email,
+    //       'phone': self.phone,
+    //     }
+    //   })
+    //   .success(function(data) {
+    //     self.clientList;
+    //   })
+    //   .error(function(data) {
+    //     console.log("cannot update")
+    //   })
+    // }
   }
   
