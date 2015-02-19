83e9d3dec7e9d42b1d50e6d3d73d7a62363e52ab
Karen S Kim <karenskim19@gmail.com>
2015-02-18 20:18:11 -0800

    add update to do function

--- app/assets/javascripts/controllers/todoscontroller.js
+++ app/assets/javascripts/controllers/todoscontroller.js
@@ -2,13 +2,14 @@ angular
   .module('app')
   .controller("TodosController", TodosController);
 
-  TodosController.$inject = [$http];
+  TodosController.$inject = ['$http'];
 
   function TodosController($http) {
     var self = this;
 
     self.todoslist = [];
     self.addTodo   = addTodo;
+    self.updateTodo   = updateTodo;
 
     $http.get("/api/todos")
       .success(function(data, status, headers, config) {
@@ -28,6 +29,26 @@ angular
       .success(function(data) {
         self.todoList;
         window.location.reload();
+      })
+      .error(function(data, status) {
+        console.log(status)
+        $('#todo-error').text("There was an issue adding this todo. Please try again.")
+      });
+    }
+
+    function updateTodo(todo) {
+      // console(todoList[$index])
+      $http({
+        url: "/api/todos/"+ todo.id,
+        method: "PATCH",
+        data: {
+          // 'task': self.task,
+          // 'due_date': self.due,
+          'done': true
+        }
+      })
+      .success(function(data) {
+        console.log("hi")
       })
       .error(function(data, status) {
         console.log(status)
