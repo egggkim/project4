0cd0f942d6777a74fecaef8f3b09bc7e4ee8d9be
Karen S Kim <karenskim19@gmail.com>
2015-03-12 13:05:02 -0700

    remove unnecessary client input validations for better user experience

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,6 +5,6 @@ class Client < ActiveRecord::Base
   # client attribute validations
   validates :first_name, presence: :true, length: { maximum: 30 }
   validates :last_name, presence: :true, length: { maximum: 30 }
-  validates :email, presence: :true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, length: { maximum: 100 }
-  validates :phone, presence: :true, format: { with: /\A\d{3}-\d{3}-\d{4}\z/ }
+  validates :email, presence: :true #, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, length: { maximum: 100 }
+  validates :phone, presence: :true #, format: { with: /\A\d{3}-\d{3}-\d{4}\z/ }
 end
